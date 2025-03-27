da9b918b54d205570dd00b01e4ff3fb3c21f8255
codewitch-honey-crisis <gaztoast@gmail.com>
2025-03-26 20:29:39 -0700

    removed debug spew from slave

--- src/slave.cpp
+++ src/slave.cpp
@@ -5,12 +5,11 @@ static bool tripped[alarm_count];
 static bool last[alarm_count];
 void setup() {
     memset(tripped,0,sizeof(bool)*alarm_count);
-    Serial.begin(115200);
     Serial2.begin(115200,SERIAL_8N1,16,17);
 }
 void loop() {
     for(int i = 0;i<alarm_count;++i) {
-        bool thrown = digitalRead(slave_pins[i])!=LOW;
+        const bool thrown = digitalRead(slave_pins[i])!=LOW;
         if(thrown!=last[i]) {
             last[i]=thrown;
             if(thrown && !tripped[i]) {
@@ -29,13 +28,11 @@ void loop() {
             case SET_ALARM:
                 if(payload[1]<alarm_count) {    
                     tripped[payload[1]]=true;
-                    printf("Set alarm %d\n",(int)payload[1]);
                 }
             break;
             case CLEAR_ALARM:
                 if(payload[1]<alarm_count) {
                     tripped[payload[1]]=false;
-                    printf("Clear alarm %d\n",(int)payload[1]);
                 }
             break;
         }
