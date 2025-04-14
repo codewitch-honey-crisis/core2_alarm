d081fa37a313379b531533cbd86cae03f4ba59b5
codewitch-honey-crisis <gaztoast@gmail.com>
2025-04-14 12:30:48 -0700

    cleanup

--- include/httpd_page.h
+++ include/httpd_page.h
@@ -1,6 +1,5 @@
 ﻿httpd_send_block("E2\r\n<!DOCTYPE html>\r\n<html>\r\n    <head>\r\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n        <title>Alarm Control Panel</title>\r\n    </head>\r\n    <body>\r\n        <form method=\"get\" action=\".\">\r\n", 232, resp_arg);
-
-           for(size_t i = 0;i<alarm_count;++i) {
+for(size_t i = 0;i<alarm_count;++i) {
             
 httpd_send_block("15\r\n\r\n            <label>\r\n", 27, resp_arg);
 httpd_send_expr(i+1, resp_arg);
@@ -10,6 +9,6 @@ httpd_send_block("2\r\n\" \r\n", 7, resp_arg);
 httpd_send_expr(alarm_values[i]?"checked":"", resp_arg);
 httpd_send_block("8\r\n/><br />\r\n", 13, resp_arg);
 
-           }
-httpd_send_block("A6\r\n\r\n            <input type=\"submit\" name=\"set\" value=\"write\"/>\r\n            <input type=\"submit\" name=\"refresh\" value=\"read\"/>\r\n        </form>\r\n    </body>\r\n</html>\r\n\r\n", 172, resp_arg);
+}
+httpd_send_block("A5\r\n\r\n            <input type=\"submit\" name=\"set\" value=\"set\" />\r\n            <input type=\"submit\" name=\"refresh\" value=\"get\" />\r\n        </form>\r\n    </body>\r\n</html>\r\n\r\n", 171, resp_arg);
 httpd_send_block("0\r\n\r\n", 5, resp_arg);
